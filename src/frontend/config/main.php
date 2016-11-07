49f591afd8f44eedfdf10c51d3d5381c236380fb
danny <dannywang@augmentum.com.cn>
2016-11-07 18:42:36 +0800

    feature(iwish): enabel url routing

--- src/frontend/config/main.php
+++ src/frontend/config/main.php
@@ -43,6 +43,7 @@ return [
             'showScriptName' => false,
             'rules' => [
                 '/' => 'site/index',
+                '<controller:\w+>/<action:[\w\/-]+>' => '<controller>/<action>',
             ],
         ],
         
